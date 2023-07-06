04b09c3ba2bd28f065fcd2e33128b4ddbda7db47
Habib <habib@HabibsMac.local>
2023-07-06 23:37:34 +0330

    vite configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import Icons from "unplugin-icons/vite";
+import svgr from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    // add any port you want
+    port: 3005,
+    host: true,
+  },
+  plugins: [
+    react(),
+    Icons({ compiler: "jsx", jsx: "react" }),
+    svgr(),
+    tsconfigPaths(),
+    visualizer({
+      emitFile: true,
+      filename: "stats.html",
+    }),
+  ],
+});
