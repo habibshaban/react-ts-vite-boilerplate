cf2318edf3827c0fbbd6e919fb248e3d983ac862
Habib <habib@HabibsMac.local>
2023-07-06 22:49:25 +0330

    config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,31 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+const defaultTheme = require("tailwindcss/defaultTheme");
 
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        "primary-light": "#ffffff",
+        "primary-dark": "#ff0000",
+        "primary-bg-light": "#ffffff",
+        "primary-bg-dark": "#0B2447",
+      },
+      screens: {},
+      transitionProperty: {},
+      animation: {},
+      keyframes: {},
+    },
+  },
+  plugins: [
+    // require('@tailwindcss/typography'),
+    // require('@tailwindcss/forms'),
+    // require('@tailwindcss/aspect-ratio'),
+    // require('@tailwindcss/container-queries'),
+  ],
+  // presets: [require("../../tailwind-preset.js")],
+};
