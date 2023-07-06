bc478159e5f3d2aba2bdd9984d3c287fc0203e3b
Habib <habib@HabibsMac.local>
2023-07-06 23:41:47 +0330

    config env files

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+  readonly VITE_PREFIX_CACHE_KEY: string;
+  readonly VITE_BASE_URL: string;
+  readonly VITE_APP_VERSION: number;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
